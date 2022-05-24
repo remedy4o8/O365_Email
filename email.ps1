8a8bee063995ddf7fbe42a03c60093dbd42a7b46
remedy4o8 <105941042+remedy4o8@users.noreply.github.com>
2022-05-24 11:27:55 -0700

    Update email.ps1

--- email.ps1
+++ email.ps1
@@ -1,4 +1,4 @@
-# This tool will change domain from .NET to .COM, enforces MFA, and add available license to tenant. 
+# This tool will change domain, enforces MFA, and add available license to tenant. 
 # Designed by Andy Pham
 
 #Module Dependency
@@ -6,31 +6,30 @@
 #AzureAD
 #Microsoft.Graph
 
-"Change arraynetworks.net to arraynetworks.com or zentrysecurity.com"
-Start-Sleep -Seconds 3
-"Connecting to Office 365..."
+Write-Host "Change Email Domains" ; Start-Sleep -Seconds 3
+Write-Host "Connecting to Office 365..."
 if(-not (Get-MsolDomain -ErrorAction SilentlyContinue))
   {
     #Start Microsoft Online Login Session
-    "Lauching Microsoft Sign In"
+    Write-Host "Lauching Microsoft Sign In"
     Import-Module MSOnline
     Connect-MsolService
-    "Login Successful"
+    Write-Host "Login Successful"
   }
 else {"You have an active Micrsoft Online Session"}
 $newEmail = (Read-Host -Prompt "Please enter new email")
 $oldEmail = (Read-Host -Prompt "Please enter old email")
 set-msoluserprincipalname -newuserprincipalname $newEmail -userprincipalname $oldEmail
-"Tenant's email has changed from $oldEmail to $newEmail"
+Write-Host "Tenant's email has changed from $oldEmail to $newEmail"
 
-"Enable Enforcing Mult-Factor Authentication"
-Start-Sleep -Seconds 3
+Write-Host "Enable Enforcing Mult-Factor Authentication" ; Start-Sleep -Seconds 3
 $mf= New-Object -TypeName Microsoft.Online.Administration.StrongAuthenticationRequirement
 $mf.RelyingParty = "*"
 $mfa = @($mf)
 Set-MsolUser -UserPrincipalName $newEmail -StrongAuthenticationRequirements $mfa
+Write-Host "Enabling MFA Complete"
 
-"Adding Microsoft 365 Business License to Tenant"
+Write-Host "Adding Microsoft 365 Business License to Tenant"
 Write-Host "Starting Microsoft Graph" ; Start-Sleep -Seconds 3
 Connect-Graph -Scopes User.ReadWrite.All, Organization.Read.All
 $PremiumSku = Get-MgSubscribedSku -All | Where-Object SkuPartNumber -eq 'O365_BUSINESS_PREMIUM'
