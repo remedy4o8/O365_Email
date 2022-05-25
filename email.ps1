9e3549c9a30bc669e8d9b222b24c9cab70f7c498
remedy4o8 <105941042+remedy4o8@users.noreply.github.com>
2022-05-25 15:23:57 -0700

    Update email.ps1

--- email.ps1
+++ email.ps1
@@ -1,26 +1,27 @@
 ## This tool will change domain, enforces MFA, and add available license to tenant. 
 ## Designed by Andy Pham
 
-## Uncomment line 5-11 to install dependency.
-# Install-Module MSOnline
-# Install-Module AzureAD
-# Install-Module Microsoft.Graph -Scope CurrentUser
+## Download, install, and import the required PowerShell modules.
+Install-Module MSOnline
+Install-Module AzureAD
+Install-Module Microsoft.Graph -Scope CurrentUser
 
-# Import-Module MSOnline
-# Import-Module AzureAD
-# Import-Module Microsoft.Graph
+Import-Module MSOnline
+Import-Module AzureAD
+Import-Module Microsoft.Graph
 
-## Changing Domain
-Write-Host "Change Email Domains" ; Start-Sleep -Seconds 3
+ ## Start Microsoft Online Login Session
 Write-Host "Connecting to Office 365..."
 if(-not (Get-MsolDomain -ErrorAction SilentlyContinue))
   {
-    ## Start Microsoft Online Login Session
     Write-Host "Lauching Microsoft Sign In"
     Connect-MsolService
     Write-Host "Login Successful"
   }
 else {"You have an active Micrsoft Online Session"}
+
+## Changing Domain
+Write-Host "Changing Email Domains" ; Start-Sleep -Seconds 3
 $newEmail = (Read-Host -Prompt "Please enter new email")
 $oldEmail = (Read-Host -Prompt "Please enter old email")
 set-msoluserprincipalname -newuserprincipalname $newEmail -userprincipalname $oldEmail
